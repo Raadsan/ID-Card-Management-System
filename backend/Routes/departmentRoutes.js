6175305b10f0fa4d6736f1716e9615a5f0b9f1d4
Abdinajib123 <cabdicabdulaahi8899@gmail.com>
2026-01-31 11:27:48 +0300

    update

--- backend/Routes/departmentRoutes.js
+++ backend/Routes/departmentRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { createDepartment, getAllDepartments, getDepartmentById, updateDepartment, deleteDepartment } from "../Controller/departmentController.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/create",createDepartment);
-router.get("/all",getAllDepartments);
-router.get("/get/:id",getDepartmentById);
-router.patch("/update/:id",updateDepartment);
-router.delete("/delete/:id",deleteDepartment);
+router.post("/", createDepartment);
+router.get("/", getAllDepartments);
+router.get("/:id", getDepartmentById);
+router.patch("/:id", updateDepartment);
+router.delete("/:id", deleteDepartment);
 
 export default router;
---
+++ backend/prisma/migrations/20260131081041_add_user_photo/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `gender` VARCHAR(191) NULL,
+    ADD COLUMN `photo` VARCHAR(191) NULL;
